e5c039f22d20cc0e27c1774a6507231143ba9d6a
Jay Shenk <jay.shenk@gmail.com>
2016-09-23 22:19:01 -0400

    Solve Stack Machine Interpretation Exercise

---
+++ medium_1/stack.js
@@ -0,0 +1,36 @@
+function minilang(commandString) {
+  var commands = commandString.split(' ');
+  var register = 0;
+  var stack = [];
+
+  commands.forEach(function(command) {
+    switch (command) {
+      case 'PUSH':
+        stack.push(register);
+        break;
+      case 'ADD':
+        register += stack.pop();
+        break;
+      case 'SUB':
+        register -= stack.pop();
+        break;
+      case 'MULT':
+        register *= stack.pop();
+        break;
+      case 'DIV':
+        register = Math.floor(register / stack.pop());
+        break;
+      case 'MOD':
+        register %= stack.pop();
+        break;
+      case 'POP':
+        register = stack.pop();
+        break;
+      case 'PRINT':
+        console.log(register);
+        break;
+      default:
+        register = Number(command);
+    }
+  });
+}
